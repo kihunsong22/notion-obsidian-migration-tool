5c85cff55a9b679a8d5991d7e25225405589ccbd
angrypig7 <angrypig777@gmail.com>
2022-10-01 17:01:11 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # notion-obsidian-migration-tool
-recommend that you don't trust this script, for it's written by a potato mixed with beer
+recommend that you don't trust this script too much, for it's written by a potato
---
+++ main.py
@@ -0,0 +1,18 @@
+import logging
+import logging.handlers
+
+logger = logging.getLogger("root")
+
+
+if __name__ == '__main__':
+    main()
+
+def main():
+    setLogging()
+
+    logger.info('notion-obsidian-migration-tool')
+    # logger.debug('')
+
+    
+
+
